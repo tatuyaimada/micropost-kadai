5bd74c2f2ee077acb510b0f819bd66d705a85d82
tatuyaimada <ti16140907@gmail.com>
2022-01-20 07:33:22 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,6 +14,11 @@
                 <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
                 {{-- ログインページへのリンク --}}
                 <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                
+                {{-- ユーザ登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
         </div>
     </nav>
